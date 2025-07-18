036c77e9bf6c2a9fb3c7dd5640df5ee209d85b12
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-07-18 12:23:24 +0200

    not found added

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Products from "./pages/Products/Products";
-import Carts from "./pages/Carts/Carts";
 import { MainLayout } from "./layout/MainLayout";
 import About from "./pages/About/About";
 import Categories from "./pages/Categories/Categories";
 import Users from "./pages/Users/Users";
 import Signup from "./pages/Signup/Signup";
 import Signin from "./pages/Signin/Signin";
+import NotFound from "./pages/NotFound/NotFound";
+import Cart from "./pages/Cart/Cart";
 
 function App() {
   return (
@@ -20,10 +21,11 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/categories" element={<Categories />} />
             <Route path="/products" element={<Products />} />
-            <Route path="/carts" element={<Carts />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="/users" element={<Users />} />
             <Route path="/sign-up" element={<Signup />} />
             <Route path="/sign-in" element={<Signin />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
